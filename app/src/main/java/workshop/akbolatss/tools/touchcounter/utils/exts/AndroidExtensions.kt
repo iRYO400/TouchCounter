4a503f33d552bc58c4f391a87d9b70df568f304b
Akbolat.SS <amaterasusan96@gmail.com>
2022-05-06 18:17:18 +0600

    Add use seconds

--- app/src/main/java/workshop/akbolatss/tools/touchcounter/utils/exts/AndroidExtensions.kt
+++ app/src/main/java/workshop/akbolatss/tools/touchcounter/utils/exts/AndroidExtensions.kt
@@ -1,10 +1,8 @@
 package workshop.akbolatss.tools.touchcounter.utils.exts
 
 import android.content.Context
-import android.content.res.Resources
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
-import kotlin.math.pow
 
 fun <T> MutableLiveData<T>.init(t: T?): MutableLiveData<T> {
     this.postValue(t)
@@ -13,13 +11,3 @@ fun <T> MutableLiveData<T>.init(t: T?): MutableLiveData<T> {
 
 fun Context.toast(message: String) =
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-
-val Int.dp: Int
-    get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
-
-infix fun Float.roundDecimals(exponent: Int): Float {
-    val expPow = 10.pow(exponent)
-    return ((this * expPow).toInt()) / expPow.toFloat()
-}
-
-infix fun Int.pow(exponent: Int): Int = toDouble().pow(exponent).toInt()
